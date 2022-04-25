1e87be68524624bfc129a01011984207630d041e
Vivek Pandey <vivek2407pandey@gmail.com>
2022-04-25 15:00:32 +0530

    section 10

---
+++ src/components/ResultsList.js
@@ -0,0 +1,27 @@
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import React from "react";
+
+const ResultsList = ({ title, results }) => {
+  return (
+    <View>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        horizontal
+        data={results}
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return <Text>{item.name}</Text>;
+        }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
+
+export default ResultsList;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,29 +1,18 @@
 import { View, Text } from "react-native";
 import React, { useState } from "react";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
-  const [results, setResults] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [searchApi, results, errorMessage] = useResults();
 
-  const searchApi = async (searchTerm) => {
-    console.log("Hi There");
-    try {
-      const response = await yelp.get("/search", {
-        params: {
-          limit: 50,
-          term: searchTerm,
-          location: "san jose",
-        },
-      });
-      setResults(response.data.businesses);
-    } catch (err) {
-      setErrorMessage("Something went wrong");
-    }
+  const filterResultsByPrice = (price) => {
+    return results.filter((result) => {
+      return result.price === price;
+    });
   };
-  //searchApi("pasta");
   return (
     <View>
       <SearchBar
@@ -33,6 +22,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice("$")} title="Cost Effective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+      <ResultsList results={filterResultsByPrice("$$$")} title="Big Spender" />
     </View>
   );
 };
